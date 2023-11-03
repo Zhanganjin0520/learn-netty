72c967270469bd877ce02b9f4713ce77a45fd38b
zhanganjin <zhanganjin0520@gmail.com>
2023-11-03 22:28:58 +0800

    mock tcp 半包/粘包 error

---
+++ src/main/java/com/netty/learn/time/server/netty/TimeServerHalfPacketHandler.java
@@ -0,0 +1,47 @@
+package com.netty.learn.time.server.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+
+/**
+ * @author Zhang Anjin
+ * @description netty time server half server handler
+ * @date 2023/10/31 22:42
+ */
+@Slf4j
+public class TimeServerHalfPacketHandler extends ChannelInboundHandlerAdapter {
+
+    //发送
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        byte[] req = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(req);
+        String body = new String(req).substring(0, req.length - System.getProperty("line.separator").length());
+        log.info("The time server receive order: {}; the counter is: {}", body, ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD REQUEST";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //将发送队列中的消息写入到 SocketChannel 中，
+        //发送给对方
+        ctx.flush();
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
